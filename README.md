6994ef07ecc4ac748acdec2616fe765d0c6e66da
Max Ogden <max@maxogden.com>
2011-05-26 17:46:25 -0700

    initial checkin

---
+++ README.md
@@ -0,0 +1,6 @@
+Replicable mapping and crowdsourcing platform!
+
+    npm install couchapp
+    couchapp push directory.js http://yourcouch/somedatabase
+
+work in progress
---
+++ directory.js
@@ -0,0 +1,32 @@
+var couchapp = require('couchapp')
+  , path = require('path')
+  ;
+
+ddoc =
+  { _id:'_design/app'
+  , rewrites :
+    [ {from:"/", to:'index.html'}
+    , {from:"/api", to:'../../'}
+    , {from:"/api/*", to:'../../*'}
+    , {from:"/*", to:'*'}
+    ]
+  }
+  ;
+
+ddoc.spatial = { 
+  all: function(doc) {
+    if(doc.name && doc.geometry) {        
+      emit(doc.geometry, doc);
+    }
+  }
+}
+
+ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {
+  if (newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
+    throw "Only admin can delete documents on this database.";
+  }
+};
+
+couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
+
+module.exports = ddoc;
